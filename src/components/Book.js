42df1ea999292f050d385f636aaefc8d104a5a99
Anthony Martin <anthonymartin20@gmail.com>
2023-02-23 09:35:28 +0100

    Setup linters

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,11 +1,18 @@
-function Book({title, author}) {
+import PropTypes from 'prop-types';
+
+function Book({ title, author }) {
   return (
     <article>
-        <h3>{title}</h3>
-        <p>{author}</p>
-        <button type="button">Remove</button>
+      <h3>{title}</h3>
+      <p>{author}</p>
+      <button type="button">Remove</button>
     </article>
   );
 }
+
+Book.propTypes = {
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+};
 
 export default Book;
--- src/routes/BooksRoute.js
+++ src/routes/BooksRoute.js
@@ -5,9 +5,9 @@ import Form from '../components/Form';
 function BooksRoute() {
   return (
     <section>
-      <Book title={"This is a book title"} author={"Anthony Martin"} />
-      <Book title={"This is another book title"} author={"Anthony Martin"} />
-      <Book title={"I don't know what to say"} author={"Anthony Martin"} />
+      <Book title="This is a book title" author="Anthony Martin" />
+      <Book title="This is another book title" author="Anthony Martin" />
+      <Book title={"I don't know what to say"} author="Anthony Martin" />
       <Form />
     </section>
   );
