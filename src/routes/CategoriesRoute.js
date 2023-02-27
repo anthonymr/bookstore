03937a1b95438f76bc01f037173f40c9a299e83e
Anthony Martin <anthonymartin20@gmail.com>
2023-02-27 20:25:38 +0100

    connect CategoriesRoute component with redux store

--- src/routes/CategoriesRoute.js
+++ src/routes/CategoriesRoute.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { checkStatus } from '../redux/categories/categoriesSlice';
 
 function CategoriesRoute() {
+  const dispatch = useDispatch();
+  const status = useSelector((status) => status.categories);
+
+  useEffect(() => {
+    dispatch(checkStatus());
+  }, [dispatch]);
+
   return (
     <section>
-      <button type="button">Check status</button>
+      <span>{status.toString()}</span>
+      <button type="button" onClick={() => dispatch(checkStatus())}>Check status</button>
     </section>
   );
 }
